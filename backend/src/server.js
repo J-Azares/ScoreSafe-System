4d5c01e2acd90298994ee7e70727067c7962ad80
J-Azares <junelazares08@gmail.com>
2026-02-12 02:48:11 +0800

    Initial commit ngani

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
