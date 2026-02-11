903e8791d3a5125540a430b2c073d69d4eec3e7a
J-Azares <junelazares08@gmail.com>
2026-02-11 17:17:02 +0800

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../../frontend')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend', 'index.html'));
+});
+
+app.listen(8080, () => {
+  console.log('Server is listening on port 8080');
+});
